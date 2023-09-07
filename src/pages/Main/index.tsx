980abb1626e4d4aa3cbb75858c6a7525fcf0f9c1
repmacrypn <artik3267@gmail.com>
2023-09-07 11:43:44 +0300

    fix: final form updates

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,14 @@
+import { Form } from 'src/components/Form'
+
+import styles from './styles.module.scss'
+
 export const Main = () => {
     return (
-        <div>
-            Main
-        </div>
+        <main className={styles.mainPageWrapper}>
+            <div className={styles.title}>
+                Заполни, пожалуйста, форму:
+            </div>
+            <Form />
+        </main>
     )
 }
