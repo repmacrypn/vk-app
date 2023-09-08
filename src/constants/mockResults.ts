1b919470000af4fffef74c5008646a8a1a4f89f5
repmacrypn <artik3267@gmail.com>
2023-09-08 15:30:09 +0300

    feat: mock data + processing timeout added

---
+++ src/constants/mockResults.ts
@@ -0,0 +1,14 @@
+export const results: IResult[] = [
+    { value: 'Бамблби', photoURL: 'https://oir.mobi/uploads/posts/2022-08/1661263747_19-oir-mobi-p-bamblbi-art-vkontakte-37.jpg' },
+    { value: 'Оптимус Прайм', photoURL: 'https://www.peoples.ru/character/movie/optimus_prime/optimus_4.jpg' },
+    { value: 'Мегатрон', photoURL: 'https://i.pinimg.com/originals/b6/e0/60/b6e0603e5b38d5586645c988f69ac939.png' },
+    { value: 'Скайскрим', photoURL: 'https://wallpapercave.com/wp/wp3483600.jpg' },
+    { value: 'Айронхайд', photoURL: 'https://news.tfw2005.com/wp-content/uploads/sites/10/2017/05/image.jpeg' },
+    { value: 'Юникрон', photoURL: 'https://i.pinimg.com/originals/d5/be/6b/d5be6b573747a222be21fcf9e3669cc6.jpg' },
+    { value: 'Джаз', photoURL: 'https://ae04.alicdn.com/kf/H013febe86684430885871d9916f6fe4aI.jpg' },
+]
+
+interface IResult {
+    value: string;
+    photoURL: string;
+}
